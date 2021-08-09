8837fc745660330eb696ef19c8170d5f8b94e479
Michael Sisson <88689056+notmikesisson@users.noreply.github.com>
2021-08-09 22:19:13 +0000

    Fixed Choose Cave function

--- Main.py
+++ Main.py
@@ -6,18 +6,14 @@ import random
 import time
 
 def displayIntro():
-	print('You are in a land full of dragons. In front of you,\n
-	you see two caves. In one cave, the dragon is friendly\n
-	and will share his treasure with you. The other dragon\n
-	is greedy and hungry, and will eat you on sight.')
+	print('You are in a land full of dragons. In front of you,\nyou see two caves. In one cave, the dragon is friendly\nand will share his treasure with you. The other dragon\nis greedy and hungry, and will eat you on sight.')
 
 def chooseCave():
-    cave = ''
-	while cave != '1' and cave != '2':
-		print('Which cave will you go into? (1 or 2)')
-		cave = input()
-
-	return caves
+  cave = ''
+  while cave != '1' and cave != '2':
+    print('Which cave will you go into? (1 or 2)')
+    cave = input()
+  return cave
 
 def checkCave(chosenCave):
 	print('You approach the cave...')
