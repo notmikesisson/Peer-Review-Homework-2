886144236441c2e1898bb8784b0f51f97898c4d4
Michael Sisson <88689056+notmikesisson@users.noreply.github.com>
2021-08-09 22:17:03 +0000

    Fixed Intro

--- Main.py
+++ Main.py
@@ -1,19 +1,15 @@
-# This is a header for the application
-# You should read this header and insert your name and your date below as part of the peer review
-# This is a typical part of any program
-# Author: <author>
-# Creation Date: <date>
+# Author: Mike Sisson
+# Creation Date: Aug 9, 2021
 # Below is a simple program with 10 issues (some are syntax errors and some are logic errors.  You need to identify the issues and correct them.
 
 import random
 import time
 
 def displayIntro():
-	print('''You are in a land full of dragons. In front of you,
-	you see two caves. In one cave, the dragon is friendly
-	and will share his treasure with you. The other dragon
-	is greedy and hungry, and will eat you on sight.''')
-	print()
+	print('You are in a land full of dragons. In front of you,\n
+	you see two caves. In one cave, the dragon is friendly\n
+	and will share his treasure with you. The other dragon\n
+	is greedy and hungry, and will eat you on sight.')
 
 def chooseCave():
     cave = ''
